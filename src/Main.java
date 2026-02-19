459872b6fe60e816aa6f92c034a6cceb198191b4
pablo ocampo <juanpabloocampoleon@gmail.com>
2026-02-18 20:59:24 -0500

    exercice : twoSum

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import Exercice.ContainsDuplicate;
-import Exercice.ContainsNearbyDuplicate;
-import Exercice.IsAnagram;
-import Exercice.LongestCommonPrefix;
+import Exercice.*;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        IsAnagram isAnagram = new IsAnagram();
-        int[] list = {1,2,3,1,2,3};
-        System.out.print(isAnagram.isAnagram("rat", "car"));
+        TwoSum twoSum = new TwoSum();
+        int[] list = {2,5,5,11};
+        
+
+        System.out.print(Arrays.toString(twoSum.twoSum(list, 10)));
     }
 }
